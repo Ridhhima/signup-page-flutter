6cc7dcd8ebfba33f01b1a263b592ca6b28ec2737
Ridhima <ridhamgoyal81@gmail.com>
2024-07-18 19:39:49 +0530

    files updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_page/db_options.dart';
 import 'package:login_page/email_password.dart';
+import 'package:login_page/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } on FirebaseException catch (e) {
+    if (e.code != 'duplicate-app') {
+      rethrow;
+    }
+  }
   runApp(const MyApp());
 }
 
